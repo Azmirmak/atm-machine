6c3680df5a205fcb58cea04aad48614c0d9957d2
Azmirmak <mazmirkhan38@gmail.com>
2024-05-04 15:14:49 +0500

    Made some changes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
-let userBalance = 25000;
+let userBalance = 30000;
 let userPin = 4477;
 console.log(`Pin for this account = ${userPin}`);
 let pinRequired = await inquirer.prompt([
     {
         name: "pin",
-        message: "Enter you card pin: ",
+        message: "Enter Your Card Pin: ",
         type: "number",
     }
 ]);
@@ -14,37 +14,49 @@ if (pinRequired.pin === userPin) {
     let operations = await inquirer.prompt([
         {
             name: "ops",
-            message: "Select the options: ",
+            message: "Select The Options: ",
             type: "list",
-            choices: ["Withdraw Cash", "Check Balance"],
+            choices: ["Withdraw Cash", "Check Balance", "Fast Cash"],
         }
     ]);
+    if (operations.ops === "Fast Cash") {
+        let fcash = await inquirer.prompt([{
+                name: "cashfast",
+                message: "Select Your Amount: ",
+                type: "list",
+                choices: ["5000", "15000", "25000"]
+            }
+        ]);
+        console.log(fcash.cashfast);
+        console.log(`You Withdrew: $ ${fcash.cashfast}`);
+    }
+    ;
     if (operations.ops === "Withdraw Cash") {
         let cash = await inquirer.prompt([
             {
                 name: "reqcash",
-                message: "Enter the amount: ",
+                message: "Enter The Amount: ",
                 type: "number",
             }
         ]);
         if (cash.reqcash > userBalance) {
-            console.log("Insufficient balance! Please check your balance and try again.");
+            console.log("Insufficient Balance! Please Check Your Balance And Try Again.");
         }
         else {
             let notes = await inquirer.prompt([{
                     name: "currency",
-                    message: "Select the currency",
+                    message: "Select The Currency (Use Space To Select And Then Press Enter): ",
                     type: "checkbox",
-                    choices: ["100", "500", "1000", "5000"],
+                    choices: ["Coins", "10", "100", "500"],
                 }
             ]);
-            console.log("You withdrew: $", cash.reqcash);
-            console.log("Currency medium: ", notes.currency);
-            console.log("Your remaining balance is: $", userBalance -= cash.reqcash);
+            console.log("You Withdrew: $", cash.reqcash);
+            console.log("Currency Medium: ", notes.currency);
+            console.log("Your Remaining Balance Is: $", userBalance -= cash.reqcash);
         }
     }
     else if (operations.ops === "Check Balance") {
-        console.log("Your current balamce is: $", userBalance);
+        console.log("Your Current Balamce Is: $", userBalance);
     }
 }
 else {
